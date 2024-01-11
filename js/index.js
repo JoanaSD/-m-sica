f2dc3011c48a6d260b52e2fbd5ff45014fa407db
Joana <joir.bilbao@alumnos.cei.es>
2024-01-11 18:25:39 +0100

    galeria
    se desborda

--- js/index.js
+++ js/index.js
@@ -3,3 +3,44 @@ let menu = document.querySelector(".container-popup");
 function popup(){
     menu.classList.toggle("active");
 }
+
+// - Variables
+
+let sliderImg = document.querySelectorAll(".slider-img.acuaciones");
+// -- Guardamos en una variable todos los elementos img que darán como valor una array.
+
+let sliderInner = document.querySelector(".slider-inner.actuaciones");
+// -- Guardamos en una variable el contenedor de las imágenes para moverlo posteriormente.
+
+let sliderActual= 0;
+// -- Creamos una variable un contador del slider actual.
+
+// 2ª PARTE
+
+// - Funciones
+
+setTimeout ( () => {
+// -- Creamos un intervalo con una función arrow.
+
+    if (sliderActual < sliderImg.length - 1) {
+    // --- Si el valor del slider actual es menor que la cantidad de img almacenadas en la variable -1:
+        sliderActual++;
+        // ---- Aumenta el valor del slider actual.
+        let calculo = sliderActual * -100;
+        // ---- Se declara una variable para hacer un cálculo que multiplique el slider actual por -100
+        sliderInner.style.transform = `translateX(${calculo}vw)`;
+        // ---- Modificamos el atributo style del contenedor de las imágenes agregandole un transform translateX con el valor de la variable anterior. Le añadimos la medida al final del resultado, por ejemplo vw.
+
+    } else if (sliderActual == sliderImg.length -1) {
+        // --- Si no es menor, pero si es el valor del slider actual igual que la cantidad de img almacenadas en la variable -1:
+
+        sliderActual = 0;
+        // ---- El valor del slider actual pasa a 0.
+        let calculo = sliderActual * -100;
+        // ---- Se declara una variable para hacer un cálculo que multiplique el slider actual por -100.
+        sliderInner.style.transform = `translateX(${calculo}vw)`;
+        // ---- Modificamos el atributo style del contenedor de las imágenes agregandole un transform translateX con el valor de la variable anterior. Le añadimos la medida al final del resultado, por ejemplo vw.
+    }
+
+}, 1000) 
+// -- Le damos un tiempo de ejecución al intervalo, por ejemplo: 5000ms
